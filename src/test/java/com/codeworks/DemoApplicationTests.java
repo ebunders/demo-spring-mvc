06a534568bcdea1d8ace3ff2f5cc78bbb9fb029a
Ernst Bunders <Ernst.Bunders@topicus.nl>
2019-02-27 09:48:13 +0100

    jenkins file added

--- src/test/java/com/codeworks/DemoApplicationTests.java
+++ src/test/java/com/codeworks/DemoApplicationTests.java
@@ -3,10 +3,12 @@ package com.codeworks;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@SpringBootTest()
+@ActiveProfiles("integration")
 public class DemoApplicationTests {
 
 	@Test
